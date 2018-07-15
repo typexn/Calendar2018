03348bd8c46d42ad3b97dac0d2549a2b3bf59b41
KyungHeeUk <35473388+typexn@users.noreply.github.com>
2018-07-15 17:45:17 +0900

    start

---
+++ app/src/main/java/com/example/kjw/a2018summerproject/sch_Mainactivity.java
@@ -0,0 +1,13 @@
+package com.example.kjw.a2018summerproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class sch_Mainactivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sch__mainactivity);
+    }
+}
