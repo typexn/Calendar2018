3dfb096fcafcb583863eef55f7be609c99e536f2
KyungHeeUk <35473388+typexn@users.noreply.github.com>
2018-08-17 14:32:18 +0900

    exercise commit

--- app/src/main/java/com/example/kjw/a2018summerproject/ex_Routine.java
+++ app/src/main/java/com/example/kjw/a2018summerproject/ex_Routine.java
@@ -2,11 +2,16 @@ package com.example.kjw.a2018summerproject;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -15,6 +20,8 @@ import java.util.ArrayList;
 
 public class ex_Routine extends Activity {
 
+    int routineCount = 0;
+    ex_ExerciseRoutine [] routineArray = new ex_ExerciseRoutine[0];
     ListView listView;
     ex_RoutineBaseAdapter ex_RoutineBaseAdapter;
 
@@ -24,12 +31,38 @@ public class ex_Routine extends Activity {
         setContentView(R.layout.activity_ex_routine);
         listView = (ListView)findViewById(R.id.ex_routine_listview);
 
-        ArrayList<ex_RoutineAdapter> list_ItemArrayList = new ArrayList<ex_RoutineAdapter>();
+        final ArrayList<ex_RoutineAdapter> list_ItemArrayList = new ArrayList<ex_RoutineAdapter>();
 
         /*  "내용에 따른 목록 추가"
         list_ItemArrayList.add()
         */
+        Button routinePlus = (Button) findViewById(R.id.ex_routine_button_routineplus);
+        routinePlus.setOnClickListener(new Button.OnClickListener(){
+            public void onClick(View v)
+            {
+                EditText routineTitle = (EditText) findViewById(R.id.ex_routine_edittext_routinetitle);
+                String value = routineTitle.getText().toString();
+                ex_ExerciseRoutine newRoutine = new ex_ExerciseRoutine(value);
+
+                ex_ExerciseRoutine[] tmpRoutine = new ex_ExerciseRoutine[routineArray.length+1];
+                System.arraycopy(routineArray,0,tmpRoutine,0,routineArray.length);
+                routineArray = tmpRoutine;
+                routineArray[routineCount] = newRoutine;
+                routineCount++;
+
+                ex_RoutineAdapter newAdapter = new ex_RoutineAdapter(R.drawable.calendar, routineArray[routineCount].routineTitle, R.drawable.calendar);
+                list_ItemArrayList.add(newAdapter);
+            }
+        });
+        Button routineToCycle = (Button) findViewById(R.id.ex_routine_listview_text_exercisetitle);
+        routineToCycle.setOnClickListener(new Button.OnClickListener(){
+            public void onClick(View v){
+                Intent routineTmp = new Intent(ex_Routine.this,ex_Cycle.class);
+                startActivity(routineTmp);
+            }
+        });
     }
+
 }
 class ex_RoutineAdapter {
     private int profile_image;
@@ -66,6 +99,7 @@ class ex_RoutineAdapter {
     public void setDelete_image(int delete_image) {
         this.delete_image = delete_image;
     }
+
 }
 
 class ex_RoutineBaseAdapter extends BaseAdapter {
@@ -112,6 +146,34 @@ class ex_RoutineBaseAdapter extends BaseAdapter {
 }
 class ex_ExerciseRoutine {
 
-    ex_ExerciseCycle tmpCycleOne = new ex_ExerciseCycle("ad","ad",3.0,5,50,20, 0);
-    ex_ExerciseCycle tmpCycletwo = new ex_ExerciseCycle("ad","ad",3.5,5,55,25, 1);
+    String routineTitle;
+
+    int cycleIndexNumber = 0;
+    ex_ExerciseCycle [] Cycle = new ex_ExerciseCycle [5];
+
+    ex_ExerciseRoutine(String routineTitle){
+        this.routineTitle =  routineTitle;
+    }
+
+    void addCycle(ex_ExerciseCycle newCycle) {
+        if(cycleIndexNumber >= Cycle.length) {
+            ex_ExerciseCycle[] tmpCycleCart = new ex_ExerciseCycle[Cycle.length*2];
+            System.arraycopy(Cycle,0,tmpCycleCart,0,Cycle.length);
+            Cycle = tmpCycleCart;
+        }
+        Cycle[cycleIndexNumber++] = newCycle;
+    }
+
+    void deleteCycle(int deleteIndexNumber){
+        for(int i = deleteIndexNumber; i < cycleIndexNumber ; i++){
+            Cycle[i].exerciseTitle = Cycle[i+1].exerciseTitle;
+            Cycle[i].exercisePart = Cycle[i+1].exercisePart;
+            Cycle[i].exerciseWeight = Cycle[i+1].exerciseWeight;
+            Cycle[i].exerciseTimeSecond = Cycle[i+1].exerciseTimeSecond;
+            Cycle[i].exerciseBreakTime = Cycle[i+1].exerciseBreakTime;
+            Cycle[i].exerciseSequence = Cycle[i+1].exerciseSequence;
+        }
+        Cycle[cycleIndexNumber] = null;
+        cycleIndexNumber--;
+    }
 }
