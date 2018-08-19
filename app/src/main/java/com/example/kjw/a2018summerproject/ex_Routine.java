9f453c956be5149b5f394c954cc45bcf00113fcb
KyungHeeUk <35473388+typexn@users.noreply.github.com>
2018-08-19 20:39:23 +0900

    adad

--- app/src/main/java/com/example/kjw/a2018summerproject/ex_Routine.java
+++ app/src/main/java/com/example/kjw/a2018summerproject/ex_Routine.java
@@ -71,7 +71,6 @@ public class ex_Routine extends Activity {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 Intent goToCycle = new Intent(ex_Routine.this, ex_Cycle.class);
-//                goToCycle.putExtra("CycleTitle", String)
                 startActivity(goToCycle);
             }
         });
