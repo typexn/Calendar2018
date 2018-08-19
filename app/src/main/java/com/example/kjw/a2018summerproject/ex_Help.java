1eacf372898d3103ce1c90cbcc47c64c778785e4
sungchanhun <cksgns93@naver.com>
2018-08-19 17:25:51 +0900

    asss

--- app/src/main/java/com/example/kjw/a2018summerproject/ex_Help.java
+++ app/src/main/java/com/example/kjw/a2018summerproject/ex_Help.java
@@ -1,13 +1,26 @@
 package com.example.kjw.a2018summerproject;
 
+import android.app.Dialog;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.LinearLayout;
 
 /**
  * Created by KJW on 2018-08-15.
  */
 
-public class ex_Help extends AppCompatActivity {
+public class ex_Help extends Dialog {
+
+
+    private LinearLayout container;
+
+
+    public ex_Help(@NonNull View.OnClickListener context) {
+        super(context);
+    }
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
