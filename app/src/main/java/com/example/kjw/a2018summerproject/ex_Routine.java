5fa9a94d1f0ace9bfc871f1ad32cd31873ed8d76
KyungHeeUk <35473388+typexn@users.noreply.github.com>
2018-08-19 20:15:44 +0900

    asssa

--- app/src/main/java/com/example/kjw/a2018summerproject/ex_Routine.java
+++ app/src/main/java/com/example/kjw/a2018summerproject/ex_Routine.java
@@ -69,8 +69,8 @@ public class ex_Routine extends Activity {
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Intent goToCycle = new Intent(ex_Routine.this, ex_Cycle.class);
-                startActivity(goToCycle);
+//                Intent goToCycle = new Intent(ex_Routine.this, ex_Cycle.class);
+//                startActivity(goToCycle);
             }
         });
     }
