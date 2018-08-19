9ec9e7beca71c185e7d1fcc1468a7c79e785c908
KyungHeeUk <35473388+typexn@users.noreply.github.com>
2018-08-19 16:23:39 +0900

    exercise!

--- app/src/main/java/com/example/kjw/a2018summerproject/ex_Routine.java
+++ app/src/main/java/com/example/kjw/a2018summerproject/ex_Routine.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.drawable.Drawable;
+import android.media.Image;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -24,20 +25,21 @@ public class ex_Routine extends Activity {
     int routineCount = 0;
     ex_ExerciseRoutine [] routineArray = new ex_ExerciseRoutine[0];
     ListView listView;
-    ex_RoutineBaseAdapter ex_RoutineBaseAdapter;
+    ex_RoutineBaseAdapter ex_RoutineBase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ex_routine);
-        listView = (ListView)findViewById(R.id.ex_routine_listview);
+        listView = (ListView)findViewById(R.id.ex_routine_listview_routine);
 
 
         final ArrayList<ex_RoutineAdapter> list_ItemArrayList = new ArrayList<ex_RoutineAdapter>();
-        final ArrayAdapter exerciseRoutineAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_single_choice,list_ItemArrayList);
+        //final ArrayAdapter exerciseRoutineAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_single_choice,list_ItemArrayList);
 
-        listView.setAdapter(exerciseRoutineAdapter);
-        listView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+        ex_RoutineBase = new ex_RoutineBaseAdapter(this, list_ItemArrayList);
+
+        listView.setAdapter(ex_RoutineBase);
 
         /*  "내용에 따른 목록 추가"
         list_ItemArrayList.add()
@@ -56,13 +58,15 @@ public class ex_Routine extends Activity {
                 routineArray[routineCount] = newRoutine;
 
                 Log.d("Uk", "routineArray.length : " + routineArray.length + "");
-
                 ex_RoutineAdapter newAdapter = new ex_RoutineAdapter(R.drawable.ic_launcher_foreground, routineArray[routineCount].routineTitle, R.drawable.ic_launcher_foreground);
-                list_ItemArrayList.add(newAdapter);
+                ex_RoutineBase.addItem(newAdapter);
+
+                Log.d("Uk", "list_itemArrayList.size : " + list_ItemArrayList.size() + "");
+
 //                int count;
 //                count = adapter.getCount();
 //                list_ItemArrayList.add("LIST" + Integer.toString(count + 1));
-                exerciseRoutineAdapter.notifyDataSetChanged();
+                ex_RoutineBase.notifyDataSetChanged();
 
                 routineCount++;
             }
@@ -77,6 +81,10 @@ public class ex_Routine extends Activity {
     }
 
 }
+
+/*
+    Routine 객체 (이미지 / 텍스트 / 이미지)
+ */
 class ex_RoutineAdapter {
     private int profile_image;
     private String title;
@@ -119,10 +127,16 @@ class ex_RoutineBaseAdapter extends BaseAdapter {
 
     Context context;
     ArrayList<ex_RoutineAdapter> list_ex_RoutineAdapter;
+    LayoutInflater mLiInflater;
 
     public ex_RoutineBaseAdapter(Context context, ArrayList<ex_RoutineAdapter> list_ex_RoutineAdapter) {
         this.context = context;
         this.list_ex_RoutineAdapter = list_ex_RoutineAdapter;
+        this.mLiInflater = (LayoutInflater) this.context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    public void addItem(ex_RoutineAdapter item){
+        list_ex_RoutineAdapter.add(item);
     }
 
     @Override
@@ -146,10 +160,11 @@ class ex_RoutineBaseAdapter extends BaseAdapter {
     @Override
     public View getView(int i, View view, ViewGroup viewGroup) {
         if(view == null) {
-            view = LayoutInflater.from(context).inflate(R.layout.ex_routine_listview, null);
+            view = mLiInflater.inflate(R.layout.ex_routine_listview, null);
+//            view = LayoutInflater.from(context).inflate(R.layout.ex_routine_listview, null);
             profile_imageView = (ImageView)view.findViewById(R.id.ex_routine_listview_image_part);
             title_textView  =(TextView)view.findViewById(R.id.ex_routine_listview_text_exercisetitle);
-            profile_imageView = (ImageView)view.findViewById(R.id.ex_routine_listview_image_delete);
+            delete_imageView = (ImageView)view.findViewById(R.id.ex_routine_listview_image_delete);
         }
         profile_imageView.setImageResource(list_ex_RoutineAdapter.get(i).getProfile_image());
         title_textView.setText(list_ex_RoutineAdapter.get(i).getTitle());
