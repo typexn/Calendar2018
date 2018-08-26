4c362610f79e23161be43c0d6f2801c1a929d89f
sungchanhun <cksgns93@naver.com>
2018-08-26 22:47:34 +0900

    qwweqw

--- app/src/main/java/com/example/kjw/a2018summerproject/ex_Mainactivity.java
+++ app/src/main/java/com/example/kjw/a2018summerproject/ex_Mainactivity.java
@@ -28,7 +28,7 @@ import java.util.Date;
  * Created by KJW on 2018-08-15.
  */
 
-public class ex_Mainactivity extends AppCompatActivity {
+public class ex_Mainactivity extends Activity implements AdapterView.OnItemClickListener, View.OnClickListener, DatePickerDialog.OnDateSetListener{
 
 
 
@@ -54,6 +54,7 @@ public class ex_Mainactivity extends AppCompatActivity {
     public static int THURSDAY = 5;
     public static int FRIDAY = 6;
     public static int SATURDAY = 7;
+    static ArrayList<Schedule> exList = new ArrayList<Schedule>(); // 일정 정보
 
     Button buttonReport;
     Button buttonHelp;
@@ -68,6 +69,21 @@ public class ex_Mainactivity extends AppCompatActivity {
         DisplayMetrics dm = getApplicationContext().getResources().getDisplayMetrics(); //디바이스 화면크기를 구하기위해
         final int width = dm.widthPixels; //디바이스 화면 너비
         final int height = dm.heightPixels; //디바이스 화면 높이
+
+        mTvCalendarTitle = (TextView) findViewById(R.id.ex_main_text_monthtitle);
+        mGvCalendar = (GridView) findViewById(R.id.ex_main_gridview_calendar);
+
+
+        mTvCalendarTitle.setOnClickListener(this);
+        mGvCalendar.setOnItemClickListener(this);
+        mDayList = new ArrayList<DayInfo>();
+
+        // 이번달 의 캘린더 인스턴스를 생성한다.
+        mThisMonthCalendar = Calendar.getInstance();
+        mThisMonthCalendar.set(Calendar.DAY_OF_MONTH, 1);
+        getCalendar(mThisMonthCalendar);
+
+        setData();
 
 
         //mainReport로 가는 버튼
@@ -108,13 +124,31 @@ public class ex_Mainactivity extends AppCompatActivity {
 
 
     }
+    private void setData() {
 
-    /*
-        현재 날짜와 같으면 ex_Routine 실행
-        이전이면 ex_lastresult 실행
-     */
-    long now = System.currentTimeMillis();
-    Date date = new Date(now);
+        for (int i = 1; i < 11; i++) {
+            Schedule ex = new Schedule("title" + i, "location" + i, 2018, 8, i, 2018, 8, i, "08", "00", "08", "00", "");
+            exList.add(ex);
+        }
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        for (int i = 0; i < exList.size(); i++) {
+            //Toast.makeText(this, exList.get(i).title, Toast.LENGTH_LONG).show();
+            for (int j = exList.get(i).startDay; j < exList.get(i).endDay + 1; j++) { //8/26~9/1 => 오류
+                if ( (exList.get(i).startMonth == (mThisMonthCalendar.get(Calendar.MONTH) + 1)) && (exList.get(i).startYear == mThisMonthCalendar.get(Calendar.YEAR))) {
+                    mCalendarAdapter.changeDayInfo(j + mThisMonthCalendar.get(Calendar.DAY_OF_WEEK) - 2, true);
+                }
+            }
+        }
+        mCalendarAdapter.notifyDataSetChanged();
+
+
+    }
 
     private void getCalendar(Calendar calendar) {
         int lastMonthStartDay;
@@ -142,23 +176,29 @@ public class ex_Mainactivity extends AppCompatActivity {
 
         lastMonthStartDay -= (dayOfMonth - 1) - 1;
         // 캘린더 타이틀(년월 표시)을 세팅한다.
-        mTvCalendarTitle.setText(mThisMonthCalendar.get(Calendar.YEAR) + "년 "
-                + (mThisMonthCalendar.get(Calendar.MONTH) + 1) + "월");
+        mTvCalendarTitle.setText(calendar.get(Calendar.YEAR) + "년 "
+                + (calendar.get(Calendar.MONTH) + 1) + "월");
         DayInfo day;
 
         Log.e("DayOfMOnth", dayOfMonth + "");
         for (int i = 0; i < dayOfMonth - 1; i++) {
             int date = lastMonthStartDay + i;
             day = new DayInfo();
+            day.setYear(calendar.get(Calendar.YEAR));
+            day.setMonth(calendar.get(Calendar.MONTH) + 1);
             day.setDay(Integer.toString(date));
             day.setInMonth(false);
+            day.setExistSch(false); //minyoung
             mDayList.add(day);
         }
 
         for (int i = 1; i <= thisMonthLastDay; i++) {
             day = new DayInfo();
+            day.setYear(calendar.get(Calendar.YEAR));
+            day.setMonth(calendar.get(Calendar.MONTH) + 1);
             day.setDay(Integer.toString(i));
             day.setInMonth(true);
+            day.setExistSch(false); //minyoung
 
             mDayList.add(day);
         }
@@ -166,8 +206,11 @@ public class ex_Mainactivity extends AppCompatActivity {
         for (int i = 1; i < 42 - (thisMonthLastDay + dayOfMonth - 1) + 1; i++) {
 
             day = new DayInfo();
+            day.setYear(calendar.get(Calendar.YEAR));
+            day.setMonth(calendar.get(Calendar.MONTH) + 1);
             day.setDay(Integer.toString(i));
             day.setInMonth(false);
+            day.setExistSch(false);//minyoung
             mDayList.add(day);
         }
         initCalendarAdapter();
@@ -205,6 +248,9 @@ public class ex_Mainactivity extends AppCompatActivity {
 
         return calendar;
     }
+
+
+
     public void onItemClick(AdapterView<?> parent, View v, int position, long arg3) {
         View convertView = ((CalendarAdapter) parent.getAdapter()).getView(position, null, null);
         CalendarAdapter.DayViewHolde dayViewHolder = (CalendarAdapter.DayViewHolde) convertView.getTag();
@@ -279,4 +325,8 @@ public class ex_Mainactivity extends AppCompatActivity {
         mGvCalendar.setAdapter(mCalendarAdapter);
     }
 
+    @Override
+    public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
+
+    }
 }
